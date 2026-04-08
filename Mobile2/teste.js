757b4144b025bd0d64f465d546b6350efce408c9
0Vidal <oliveira.vidal.vinicius@escola.pr.gov.br>
2026-04-08 11:52:58 -0300

    Add files via upload

---
+++ Mobile2/teste.js
@@ -0,0 +1,44 @@
+//1.Exiba a mensagem "Olá Mundo" no console.
+console.log("Olá Mundo!");
+
+//2.Crie uma variável com seu nome e exiba um valor no console.
+let nome = "Vinícius";
+console.log(nome); 
+
+//3.Some dois números e exiba o resultado
+n1 = 20;
+n2 = 20;
+let adicao = n1 + n2; 
+console.log(adicao)
+
+//Resolução do professor
+let soma = 10 + 5;
+console.log(soma);
+
+//4.Subtraia dois números e exiba o valor.
+let sub = 10 - 5;
+console.log(sub);
+
+//5.Descubra o resto da divsão de um número por outro.
+let div = 15 % 5;
+console.log(div);
+
+//6.Crie uma variável booleana (true ou false) e exiba o valor.
+let verdade = true;
+console.log("Verdade:", verdade);
+
+//7.Verifique se um número é maior que outro e exiba o resultado.
+n1 = 10;
+n2 = 5;
+if(n1 > n2){
+    console.log("O número é maior");
+}
+else{
+    console.log("O número é menor");
+}
+
+//Resolução o Professor.
+let MaiorQue = 18 > 11;
+console.log(MaiorQue);
+
+//8.
